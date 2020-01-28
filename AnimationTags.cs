a49316044df0bbd98d434d8c892213e3ef8f229a
jakeburton01 <44500244+jakeburton01@users.noreply.github.com>
2020-01-28 18:18:39 +0000

    Updated Health scripts working with Punches

---
+++ AnimationTags.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTags : MonoBehaviour
+{
+   
+
+
+}
+
+public class Tags
+{
+    public const string LEFT_PUNCH_TRIGGER = "LeftPunch";
+    public const string RIGHT_PUNCH_TRIGGER = "RightPunch";
+    public const string STRONG_PUNCH_TRIGGER = "StrongPunch";
+
+    public const string MOVEMENT = "Movement";
+
+    public const string HIT_ANIMATION = "Hit";
+    public const string DIZZY_ANIMATION = "Dizzy";
+    public const string IDLE_ANIMATION = "Idle";
+
+    public const string LEFT_PUNCH_TAG = "LeftPunchTag";
+    public const string RIGHT_PUNCH_TAG = "RightPunchTag";
+    public const string STRONG_PUNCH_TAG = "StrongPunchTag";
+    public const string UNTAGGED_TAG = "UnTag";
+
+}
+
+public class Axis
+{
+    public const string HORIZONTAL_AXIS = "Horizontal";
+    public const string VERTICAL_AXIS = "Vertical";
+}
---
+++ AttackUniversal.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackUniversal : MonoBehaviour {
+
+    public LayerMask collisionLayer;
+    public float radius = 1f;
+    public float damage = 2f;
+
+
+
+    public bool is_Player, is_Enemy;
+
+    public GameObject hit_FX;
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetectCollision();
+    }
+
+    void DetectCollision()
+    {
+        Collider[] hit = Physics.OverlapSphere(transform.position, radius, collisionLayer);
+        
+        if (hit.Length > 0)
+        {
+            
+            if (is_Player)
+            {
+                
+                if (gameObject.CompareTag(Tags.LEFT_PUNCH_TAG) ||
+                    gameObject.CompareTag(Tags.RIGHT_PUNCH_TAG)) {
+                    
+                    hit[0].GetComponent<Healthblocks>().NormalDamage();
+                }
+               
+            }
+            gameObject.SetActive(false);
+        }
+
+
+    }
+
+}
