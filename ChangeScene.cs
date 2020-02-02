928468383f21caaa35dd6ed9dbb466f967686570
sk9647f <36513930+sk9647f@users.noreply.github.com>
2020-02-02 14:45:47 +0000

    Add files via upload
    Requires a folder in assets named TextFiles with a file named SceneChange.txt
    Assets/TextFiles/SceneChange.txt
    
    Writing to the document isn't implemented as i'm unsure how our menus will work.
    Will be a script called when a start button is pressed to move into a new scene. Will activate/deactivate the appropriate player/AI game objects depending on what the player chose.

---
+++ ChangeScene.cs
@@ -0,0 +1,240 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using System.IO;
+
+
+public class ChangeScene : MonoBehaviour
+{
+    public string path;
+    public string tempRead;
+    public string readOut;
+
+    public GameObject TempGameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        path = Application.dataPath.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("l"))
+        {
+            ReadString("");
+        }
+    }
+
+    public void ReadString(string input)
+    {
+
+        path = "Assets/TextFiles/SceneChange.txt";
+        StreamReader reader = new StreamReader(path);
+        readOut = reader.ReadLine();
+        reader.Close();
+        tempRead = readOut[0].ToString();
+        print(readOut);
+        print(tempRead);
+        InitializeObjects(readOut);
+    }
+
+    public void InitializeObjects(string input)
+    {
+        string x = input;
+
+        x = input[0].ToString();
+        ActivatePlayers(x);
+
+        x = input[1].ToString();
+        ActivateAI(x);
+
+    }
+
+
+    public void ActivatePlayers(string input)
+    {
+        string x = input;
+
+        if(x == "0")
+        {
+            return;
+        }
+        if (x == "1")
+        {
+            TempGameObject = GameObject.Find("Player1");
+            TempGameObject.gameObject.SetActive(true);
+            TempGameObject = null;
+        }
+        if (x == "2")
+        {
+            TempGameObject = GameObject.Find("Player1");
+            TempGameObject.gameObject.SetActive(true);
+
+            TempGameObject = GameObject.Find("Player2");
+            TempGameObject.gameObject.SetActive(true);
+
+            TempGameObject = null;
+        }
+
+        if (x == "3")
+        {
+            TempGameObject = GameObject.Find("Player1");
+            TempGameObject.gameObject.SetActive(true);
+
+            TempGameObject = GameObject.Find("Player2");
+            TempGameObject.gameObject.SetActive(true);
+
+            TempGameObject = GameObject.Find("Player3");
+            TempGameObject.gameObject.SetActive(true);
+
+            TempGameObject = null;
+        }
+
+        if (x == "4")
+        {
+            TempGameObject = GameObject.Find("Player1");
+            TempGameObject.gameObject.SetActive(true);
+
+            TempGameObject = GameObject.Find("Player2");
+            TempGameObject.gameObject.SetActive(true);
+
+            TempGameObject = GameObject.Find("Player3");
+            TempGameObject.gameObject.SetActive(true);
+
+            TempGameObject = GameObject.Find("Player4");
+            TempGameObject.gameObject.SetActive(true);
+
+            TempGameObject = null;
+        }
+
+    }
+
+
+    public void ActivateAI(string input)
+    {
+        string x = input;
+
+        if(x == "0")
+        {
+            TempGameObject = GameObject.Find("AI1");
+            TempGameObject.SetActive(false);
+
+            TempGameObject = GameObject.Find("AI2");
+            TempGameObject.SetActive(false);
+
+            TempGameObject = GameObject.Find("AI3");
+            TempGameObject.SetActive(false);
+
+            TempGameObject = GameObject.Find("AI4");
+            TempGameObject.SetActive(false);
+            return;
+        }
+        if(x == "1")
+        {
+            TempGameObject = GameObject.Find("AI1");
+            TempGameObject.transform.position = new Vector3(6, 0, 0);
+            TempGameObject.GetComponentInChildren<AI>().enabled = true;
+            TempGameObject.GetComponentInChildren<NavMeshAgent>().enabled = true;
+
+            TempGameObject = GameObject.Find("AI2");
+            TempGameObject.SetActive(false);
+            
+
+            TempGameObject = GameObject.Find("AI3");
+            TempGameObject.SetActive(false);
+
+            TempGameObject = GameObject.Find("AI4");
+            TempGameObject.SetActive(false);
+
+            TempGameObject = null;
+        }
+        if (x == "2")
+        {
+            TempGameObject = GameObject.Find("AI1");
+            TempGameObject.transform.position = new Vector3(6, 0, 0);
+            TempGameObject.GetComponentInChildren<AI>().enabled = true;
+            TempGameObject.GetComponentInChildren<NavMeshAgent>().enabled = true;
+
+            TempGameObject = GameObject.Find("AI2");
+            TempGameObject.transform.position = new Vector3(-5, 0, 0);
+            TempGameObject.GetComponentInChildren<AI>().enabled = true;
+            TempGameObject.GetComponentInChildren<NavMeshAgent>().enabled = true;
+
+            TempGameObject = GameObject.Find("AI3");
+            TempGameObject.SetActive(false);
+
+            TempGameObject = GameObject.Find("AI4");
+            TempGameObject.SetActive(false);
+
+            TempGameObject = null;
+        }
+        if (x == "3")
+        {
+            TempGameObject = GameObject.Find("AI1");
+            TempGameObject.transform.position = new Vector3(6, 0, 0);
+            TempGameObject.GetComponentInChildren<AI>().enabled = true;
+            TempGameObject.GetComponentInChildren<NavMeshAgent>().enabled = true;
+
+            TempGameObject = GameObject.Find("AI2");
+            TempGameObject.transform.position = new Vector3(-5, 0, 0);
+            TempGameObject.GetComponentInChildren<AI>().enabled = true;
+            TempGameObject.GetComponentInChildren<NavMeshAgent>().enabled = true;
+
+            TempGameObject = GameObject.Find("AI3");
+            TempGameObject.transform.position = new Vector3(5, 0, 8);
+            TempGameObject.GetComponentInChildren<AI>().enabled = true;
+            TempGameObject.GetComponentInChildren<NavMeshAgent>().enabled = true;
+
+            TempGameObject = GameObject.Find("AI4");
+            TempGameObject.SetActive(false);
+
+            TempGameObject = null;
+        }
+        if (x == "4")
+        {
+            TempGameObject = GameObject.Find("AI1");
+            TempGameObject.transform.position = new Vector3(6, 0, 0);
+            TempGameObject.GetComponentInChildren<AI>().enabled = true;
+            TempGameObject.GetComponentInChildren<NavMeshAgent>().enabled = true;
+
+            TempGameObject = GameObject.Find("AI2");
+            TempGameObject.transform.position = new Vector3(-5, 0, 0);
+            TempGameObject.GetComponentInChildren<AI>().enabled = true;
+            TempGameObject.GetComponentInChildren<NavMeshAgent>().enabled = true;
+
+            TempGameObject = GameObject.Find("AI3");
+            TempGameObject.transform.position = new Vector3(5, 0, 8);
+            TempGameObject.GetComponentInChildren<AI>().enabled = true;
+            TempGameObject.GetComponentInChildren<NavMeshAgent>().enabled = true;
+
+            TempGameObject = GameObject.Find("AI4");
+            TempGameObject.transform.position = new Vector3(-5, 0, 8);
+            TempGameObject.GetComponentInChildren<AI>().enabled = true;
+            TempGameObject.GetComponentInChildren<NavMeshAgent>().enabled = true;
+            
+
+            TempGameObject = null;
+        }
+    }
+
+    public void WriteString(string input)
+    {
+        path = "Assets/TextFiles/SceneChange.txt";
+
+        using (var stream = new FileStream(path, FileMode.Truncate))
+        {
+            using (var writer = new StreamWriter(stream))
+            {
+                writer.Write(input);
+            }
+        }
+    }
+
+
+
+}
