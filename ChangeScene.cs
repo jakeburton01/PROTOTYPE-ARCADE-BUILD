ba6946f4f1d26a493c5c733cca3019ed04d9ce70
sk9647f <36513930+sk9647f@users.noreply.github.com>
2020-02-09 13:13:10 +0000

    Update ChangeScene.cs

--- ChangeScene.cs
+++ ChangeScene.cs
@@ -19,6 +19,10 @@ public class ChangeScene : MonoBehaviour
     public GameObject TempGameObject;
     public Text HoldText;
     public Text TempText;  //Above UI objects are temporary for my specific test scene, will change dependant on final menu
+
+
+    public GameObject AIprefab;
+    public GameObject PlayerPrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -42,7 +46,7 @@ public class ChangeScene : MonoBehaviour
 	
         HoldText.text = HoldText.text + dropdown1.value.ToString();//Temporary UI objects that contain text values that can be pulled out
         HoldText.text = HoldText.text + dropdown2.value.ToString(); //Joins the two strings together in another UI text 
-        print(HoldText.text);  //DEBUG
+        //print(HoldText.text);  //DEBUG
         WriteString(HoldText.text); //Pushes the two joined strings into the WriteString function
 
         SceneManager.LoadScene("SampleScene"); //Changes scene to the arena scene
@@ -72,8 +76,8 @@ public class ChangeScene : MonoBehaviour
         readOut = reader.ReadToEnd(); //Reads the entire document and sets the output string to readOut
         reader.Close(); //Closes and removes the reader from memory    #####Important#####
         tempRead = readOut[1].ToString(); // DEBUG
-        print(readOut.Length); // DEBUG
-        print(tempRead);//DEBUG
+        //print(readOut.Length); // DEBUG
+       // print(tempRead);//DEBUG
         InitializeObjects(readOut); //Takes the string and moves into InitializeObjects function
     }
 
@@ -82,7 +86,7 @@ public class ChangeScene : MonoBehaviour
         string x = input; //Sets the input parameter to the previously read output string    (I don't know why I defined it straight to input but)
 
         x = input[1].ToString();  //Takes the second character in the string and sets it to x     (For some reason there's always a space in the 0 slot, haven't fixed it)
-        ActivatePlayers(x);  //This will then take the character and move into ActivatePlayers function
+       // ActivatePlayers(x);  //This will then take the character and move into ActivatePlayers function
 
         x = input[2].ToString();
         ActivateAI(x); //Same as above but for third character, 2 in array, and into ActivateAI rather than player
@@ -96,85 +100,62 @@ public class ChangeScene : MonoBehaviour
 
         if(x == "0")
         {
-			TempGameObject = GameObject.Find("Player1"); //TempGameObject is set to find the Player1 object in the scene      (GameObject.find() Can only find active objects in scene)
-            TempGameObject.gameObject.SetActive(false); // Object is then deactivated
 			
-            TempGameObject = GameObject.Find("Player2");
-            TempGameObject.gameObject.SetActive(false);
-			
-			TempGameObject = GameObject.Find("Player3");
-            TempGameObject.gameObject.SetActive(false);
-			
-			TempGameObject = GameObject.Find("Player4");
-            TempGameObject.gameObject.SetActive(false);
-			
+            
 			TempGameObject = null; //TempGameObject is then set to null for next sweep
 			
         }
         if (x == "1")
         {
-            TempGameObject = GameObject.Find("Player1"); //Finds the correct object
-            TempGameObject.transform.position = new Vector3(5, 0, 0); //Moves it to its correct location
-			
-			TempGameObject = GameObject.Find("Player2");
-            TempGameObject.gameObject.SetActive(false);
-			
-			TempGameObject = GameObject.Find("Player3");
-            TempGameObject.gameObject.SetActive(false);
-			
-			TempGameObject = GameObject.Find("Player4");
-            TempGameObject.gameObject.SetActive(false);
+
+            TempGameObject = GameObject.Instantiate(PlayerPrefab, new Vector3(5, 0, 0), Quaternion.identity);
+            TempGameObject.name = "Player1";
+            
 			
 			
             TempGameObject = null;
         }
         if (x == "2")
         {
-            TempGameObject = GameObject.Find("Player1");
-            TempGameObject.transform.position = new Vector3(5, 0, 0);
-			
-			TempGameObject = GameObject.Find("Player2");
-            TempGameObject.transform.position = new Vector3(-5, 0, 0);
-			
-			TempGameObject = GameObject.Find("Player3");
-            TempGameObject.gameObject.SetActive(false);
-			
-			TempGameObject = GameObject.Find("Player4");
-            TempGameObject.gameObject.SetActive(false);
+            TempGameObject = GameObject.Instantiate(PlayerPrefab, new Vector3(5, 0, 0), Quaternion.identity);
+
+            TempGameObject = GameObject.Instantiate(PlayerPrefab, new Vector3(-5, 0, 0), Quaternion.identity);
+            TempGameObject.name = "Player2";
+
+           
 
             TempGameObject = null;
         }
 
         if (x == "3")
         {
-            TempGameObject = GameObject.Find("Player1");
-            TempGameObject.transform.position = new Vector3(5, 0, 0);
-			
-			TempGameObject = GameObject.Find("Player2");
-            TempGameObject.transform.position = new Vector3(-5, 0, 0);
-			
-			TempGameObject = GameObject.Find("Player3");
-            TempGameObject.transform.position = new Vector3(5, 0, 8);
-			
-			TempGameObject = GameObject.Find("Player4");
-            TempGameObject.gameObject.SetActive(false);
+            TempGameObject = GameObject.Instantiate(PlayerPrefab, new Vector3(5, 0, 0), Quaternion.identity);
+            TempGameObject.name = "Player1";
+
+            TempGameObject = GameObject.Instantiate(PlayerPrefab, new Vector3(-5, 0, 0), Quaternion.identity);
+            TempGameObject.name = "Player2";
+
+            TempGameObject = GameObject.Instantiate(PlayerPrefab, new Vector3(5, 0, 8), Quaternion.identity);
+            TempGameObject.name = "Player3";
+
+            
 
             TempGameObject = null;
         }
 
         if (x == "4")
         {
-            TempGameObject = GameObject.Find("Player1");
-            TempGameObject.transform.position = new Vector3(5, 0, 0);
-			
-			TempGameObject = GameObject.Find("Player2");
-            TempGameObject.transform.position = new Vector3(-5, 0, 0);
-			
-			TempGameObject = GameObject.Find("Player3");
-            TempGameObject.transform.position = new Vector3(5, 0, 8);
-			
-			TempGameObject = GameObject.Find("Player4");
-            TempGameObject.transform.position = new Vector3(-5, 0, 8);
+            TempGameObject = GameObject.Instantiate(PlayerPrefab, new Vector3(5, 0, 0), Quaternion.identity);
+            TempGameObject.name = "Player1";
+
+            TempGameObject = GameObject.Instantiate(PlayerPrefab, new Vector3(-5, 0, 0), Quaternion.identity);
+            TempGameObject.name = "Player2";
+
+            TempGameObject = GameObject.Instantiate(PlayerPrefab, new Vector3(5, 0, 8), Quaternion.identity);
+            TempGameObject.name = "Player3";
+
+            TempGameObject = GameObject.Instantiate(PlayerPrefab, new Vector3(-5, 0, 8), Quaternion.identity);
+            TempGameObject.name = "Player4";
 
             TempGameObject = null;
         }
@@ -188,102 +169,81 @@ public class ChangeScene : MonoBehaviour
 
         if(x == "0")
         {
-            TempGameObject = GameObject.Find("AI1");
-            TempGameObject.SetActive(false); // Same as above
-
-            TempGameObject = GameObject.Find("AI2");
-            TempGameObject.SetActive(false);
-
-            TempGameObject = GameObject.Find("AI3");
-            TempGameObject.SetActive(false);
-
-            TempGameObject = GameObject.Find("AI4");
-            TempGameObject.SetActive(false);
+            
             return;
         }
         if(x == "1")
         {
-            TempGameObject = GameObject.Find("AI1");
-            TempGameObject.transform.position = new Vector3(5, 0, 0); // Same as above
-            TempGameObject.GetComponentInChildren<AI>().enabled = true; //For AI units their NavMeshAgent and AI components need to be enabled after they're moved into their correct positions, otherwise the AI and NavMeshAgent try to function and throw a bunch of errors and issues.
-            TempGameObject.GetComponentInChildren<NavMeshAgent>().enabled = true;
+            TempGameObject = GameObject.Instantiate(AIprefab, new Vector3(5, 0, 0), Quaternion.identity); //TempGameObject is set to find the Player1 object in the scene      (GameObject.find() Can only find active objects in scene)
+            AI TempAICall = TempGameObject.GetComponent<AI>();
+            TempAICall.InitializeVariables(1, 2, 5, 5, "AI1");
+            TempAICall.enabled = true;
 
-            TempGameObject = GameObject.Find("AI2");
-            TempGameObject.SetActive(false);
-            
 
-            TempGameObject = GameObject.Find("AI3");
-            TempGameObject.SetActive(false);
-
-            TempGameObject = GameObject.Find("AI4");
-            TempGameObject.SetActive(false);
+           
 
             TempGameObject = null;
         }
         if (x == "2")
         {
-            TempGameObject = GameObject.Find("AI1");
-            TempGameObject.transform.position = new Vector3(5, 0, 0);
-            TempGameObject.GetComponentInChildren<AI>().enabled = true;
-            TempGameObject.GetComponentInChildren<NavMeshAgent>().enabled = true;
+            TempGameObject = GameObject.Instantiate(AIprefab, new Vector3(5, 0, 0), Quaternion.identity); //TempGameObject is set to find the Player1 object in the scene      (GameObject.find() Can only find active objects in scene)
+            AI TempAICall = TempGameObject.GetComponent<AI>();
+            TempAICall.InitializeVariables(1, 2, 5, 5, "AI1");
+            TempAICall.enabled = true;
 
-            TempGameObject = GameObject.Find("AI2");
-            TempGameObject.transform.position = new Vector3(-5, 0, 0);
-            TempGameObject.GetComponentInChildren<AI>().enabled = true;
-            TempGameObject.GetComponentInChildren<NavMeshAgent>().enabled = true;
+            
+            TempGameObject = GameObject.Instantiate(AIprefab, new Vector3(-5, 0, 0), Quaternion.identity); //TempGameObject is set to find the Player1 object in the scene      (GameObject.find() Can only find active objects in scene)
+            AI TempAICall2 =  TempGameObject.GetComponent<AI>();
+            TempAICall2.InitializeVariables(2, 2, 5, 5, "AI2");
+            TempAICall2.enabled = true;
 
-            TempGameObject = GameObject.Find("AI3");
-            TempGameObject.SetActive(false);
-
-            TempGameObject = GameObject.Find("AI4");
-            TempGameObject.SetActive(false);
 
             TempGameObject = null;
         }
         if (x == "3")
         {
-            TempGameObject = GameObject.Find("AI1");
-            TempGameObject.transform.position = new Vector3(5, 0, 0);
-            TempGameObject.GetComponentInChildren<AI>().enabled = true;
-            TempGameObject.GetComponentInChildren<NavMeshAgent>().enabled = true;
+            TempGameObject = GameObject.Instantiate(AIprefab, new Vector3(5, 0, 0), Quaternion.identity); //TempGameObject is set to find the Player1 object in the scene      (GameObject.find() Can only find active objects in scene)
+            AI TempAICall = TempGameObject.GetComponent<AI>();
+            TempAICall.InitializeVariables(1, 2, 5, 5, "AI1");
+            TempAICall.enabled = true;
 
-            TempGameObject = GameObject.Find("AI2");
-            TempGameObject.transform.position = new Vector3(-5, 0, 0);
-            TempGameObject.GetComponentInChildren<AI>().enabled = true;
-            TempGameObject.GetComponentInChildren<NavMeshAgent>().enabled = true;
 
-            TempGameObject = GameObject.Find("AI3");
-            TempGameObject.transform.position = new Vector3(5, 0, 8);
-            TempGameObject.GetComponentInChildren<AI>().enabled = true;
-            TempGameObject.GetComponentInChildren<NavMeshAgent>().enabled = true;
+            TempGameObject = GameObject.Instantiate(AIprefab, new Vector3(-5, 0, 0), Quaternion.identity); //TempGameObject is set to find the Player1 object in the scene      (GameObject.find() Can only find active objects in scene)
+            AI TempAICall2 = TempGameObject.GetComponent<AI>();
+            TempAICall2.InitializeVariables(2, 2, 5, 5, "AI2");
+            TempAICall2.enabled = true;
 
-            TempGameObject = GameObject.Find("AI4");
-            TempGameObject.SetActive(false);
+            TempGameObject = GameObject.Instantiate(AIprefab, new Vector3(5, 0, 8), Quaternion.identity); //TempGameObject is set to find the Player1 object in the scene      (GameObject.find() Can only find active objects in scene)
+            AI TempAICall3 = TempGameObject.GetComponent<AI>();
+            TempAICall3.InitializeVariables(3, 2, 5, 5, "AI3");
+            TempAICall3.enabled = true;
+
 
             TempGameObject = null;
         }
         if (x == "4")
         {
-            TempGameObject = GameObject.Find("AI1");
-            TempGameObject.transform.position = new Vector3(5, 0, 0);
-            TempGameObject.GetComponentInChildren<AI>().enabled = true;
-            TempGameObject.GetComponentInChildren<NavMeshAgent>().enabled = true;
+            TempGameObject = GameObject.Instantiate(AIprefab, new Vector3(5, 0, 0), Quaternion.identity); //TempGameObject is set to find the Player1 object in the scene      (GameObject.find() Can only find active objects in scene)
+            AI TempAICall = TempGameObject.GetComponent<AI>();
+            TempAICall.InitializeVariables(1, 2, 5, 5, "AI1");
+            TempAICall.enabled = true;
 
-            TempGameObject = GameObject.Find("AI2");
-            TempGameObject.transform.position = new Vector3(-5, 0, 0);
-            TempGameObject.GetComponentInChildren<AI>().enabled = true;
-            TempGameObject.GetComponentInChildren<NavMeshAgent>().enabled = true;
 
-            TempGameObject = GameObject.Find("AI3");
-            TempGameObject.transform.position = new Vector3(5, 0, 8);
-            TempGameObject.GetComponentInChildren<AI>().enabled = true;
-            TempGameObject.GetComponentInChildren<NavMeshAgent>().enabled = true;
+            TempGameObject = GameObject.Instantiate(AIprefab, new Vector3(-5, 0, 0), Quaternion.identity); //TempGameObject is set to find the Player1 object in the scene      (GameObject.find() Can only find active objects in scene)
+            AI TempAICall2 = TempGameObject.GetComponent<AI>();
+            TempAICall2.InitializeVariables(2, 2, 5, 5, "AI2");
+            TempAICall2.enabled = true;
 
-            TempGameObject = GameObject.Find("AI4");
-            TempGameObject.transform.position = new Vector3(-5, 0, 8);
-            TempGameObject.GetComponentInChildren<AI>().enabled = true;
-            TempGameObject.GetComponentInChildren<NavMeshAgent>().enabled = true;
-            
+            TempGameObject = GameObject.Instantiate(AIprefab, new Vector3(5, 0, 8), Quaternion.identity); //TempGameObject is set to find the Player1 object in the scene      (GameObject.find() Can only find active objects in scene)
+            AI TempAICall3 = TempGameObject.GetComponent<AI>();
+            TempAICall3.InitializeVariables(3, 2, 5, 5, "AI3");
+            TempAICall3.enabled = true;
+
+            TempGameObject = GameObject.Instantiate(AIprefab, new Vector3(-5, 0, 8), Quaternion.identity); //TempGameObject is set to find the Player1 object in the scene      (GameObject.find() Can only find active objects in scene)
+            AI TempAICall4 = TempGameObject.GetComponent<AI>();
+            TempAICall4.InitializeVariables(4, 2, 5, 5, "AI3");
+            TempAICall4.enabled = true;
+
 
             TempGameObject = null;
         }
