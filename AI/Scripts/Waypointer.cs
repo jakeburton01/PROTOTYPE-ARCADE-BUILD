a3a026db69d55c3010e81b63c47226241abf954a
sk9647f <36513930+sk9647f@users.noreply.github.com>
2020-03-03 14:59:51 +0000

    Add files via upload

---
+++ AI/Scripts/Waypointer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypointer : MonoBehaviour
+{
+	public bool Reset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Reset = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void OnTriggerEnter(Collider collision)
+	{
+        print("Waypoint hit");
+		if (collision.gameObject.tag == "Enemy")
+		{
+			Reset = true;
+            print("Waypoint Reset");
+		}
+	}
+}
